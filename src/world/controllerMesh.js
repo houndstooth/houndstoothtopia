1c260adf6a80c58c6788c91ed0d6014d2e3ce537
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-03-17 19:50:04 +0900

    ensure consistent ordering of the geometries the controller cycles through

--- src/world/controllerMesh.js
+++ src/world/controllerMesh.js
@@ -6,12 +6,10 @@ import {CONTROLLER_SCALE} from '../space'
 const CONTROLLER_ROTATION = [0, Math.PI / 4, 0]
 
 const controllerMesh = index => {
-    const geometryKeys = Object.keys(geometry)
-    const geometryKeyIndex = index % (geometryKeys.length - 1) + 1
-    const geometryKey = geometryKeys[geometryKeyIndex]
-    const geo = geometry[geometryKey]
-
-    const mesh = new Mesh(geo, new MeshLambertMaterial({color: CONTROLLER_COLOR}))
+    const mesh = new Mesh(
+        geometry.byIndex[index % geometry.byIndex.length],
+        new MeshLambertMaterial({color: CONTROLLER_COLOR}),
+    )
     mesh.rotation.set(...CONTROLLER_ROTATION)
     mesh.scale.set(...CONTROLLER_SCALE)
 
--- src/world/geometry.js
+++ src/world/geometry.js
@@ -2,13 +2,17 @@ import {OBJLoader} from 'threejs-full-es6'
 
 const context = require.context('../../geometry')
 
+const byIndex = []
+
 const load = async () => {
     await Promise.all(
-        context.keys().map(path => {
+        context.keys().map((path, index) => {
             return new Promise(resolve => {
                 new OBJLoader().load(context(path), object => {
                     const name = path.replace('./', '').replace('.obj', '')
-                    geometry[name] = object.children[0].geometry
+                    const result = object.children[0].geometry
+                    geometry[name] = result
+                    byIndex[index] = result
                     resolve()
                 })
             })
@@ -18,6 +22,7 @@ const load = async () => {
 
 const geometry = {
     load,
+    byIndex,
 }
 
 export {
