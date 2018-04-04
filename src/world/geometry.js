c7a9c8eb04fca2b5e7e1ac765d5ee0af5b1bb9b5
Douglas Blumeyer <dblumeyer@pivotal.io>
2018-04-04 12:52:49 +0900

    finally successfully mute threejs mandatory logging urggh
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- src/world/geometry.js
+++ src/world/geometry.js
@@ -1,5 +1,10 @@
 import {OBJLoader} from 'threejs-full-es6'
 
+console.oldTime = console.time
+console.time = (...args) => args[0] !== 'OBJLoader' && console.oldTime.apply(console, args)
+console.oldTimeEnd = console.timeEnd
+console.timeEnd = (...args) => args[0] !== 'OBJLoader' && console.oldTimeEnd.apply(console, args)
+
 const context = require.context('../../geometry')
 
 const geometry = {
