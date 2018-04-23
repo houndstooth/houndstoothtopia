85ed9a6966e30d3b62e780fb37e8154bca73e484
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-04-23 13:04:09 +0900

    make audio not reliant on geometry

--- src/audio/assignSpeaker.js
+++ src/audio/assignSpeaker.js
@@ -1,21 +1,26 @@
 import {items} from '../world/items'
+import {Object3D} from 'threejs-full-es6'
+
+const MAX_TRIES = 30
 
 const assignSpeaker = () => {
     let success = false
     let specificItem
-    while (!success) {
+    let tries = 0
+    while (tries < MAX_TRIES) {
+        tries++
         specificItem = randomElement(randomValue(items))
-        if (!specificItem.assigned) {
+        if (specificItem && !specificItem.assigned) {
             specificItem.assigned = true
             success = true
         }
     }
 
-    return specificItem
+    return specificItem || new Object3D()
 }
 
-const randomElement = array => array[Math.floor(Math.random() * array.length)]
+const randomElement = array => array && array.length && array[Math.floor(Math.random() * array.length)]
 
-const randomValue = object => object[randomElement(Object.keys(object))]
+const randomValue = object => Object.keys(object).length && object[randomElement(Object.keys(object))]
 
 export default assignSpeaker
