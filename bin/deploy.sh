f10e48cd07b9e96b9287daec8ed8488fbad9b579
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-05 14:54:34 -0800

    deploy to gcp

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env sh
 
-cf a > /dev/null
-if [[ $? == 1 ]] ; then
-    if [[ -z "${PWS_ORG+x}" ]] ; then
-        printf "${Cyan}Please set PWS_ORG, PWS_SPACE, PWS_USERNAME, and PWS_PASSWORD environment variables if you would like to automatically push to a Cloud Foundry.\n${NC}"
-        exit 1
-    else
-        cf login -a api.run.pivotal.io -o "$PWS_ORG" -s "$PWS_SPACE" -u "$PWS_USERNAME" -p "$PWS_PASSWORD"
-    fi
-fi
-
-NODE_ENV=production webpack
-#cf zero-downtime-push houndstoothtopia -f manifest.yml
-gloud app deploy --project houndstoothtopia
+NODE_ENV=production ./node_modules/.bin/webpack
+gcloud config configurations activate houndstoothtopia
+gcloud app deploy -q
