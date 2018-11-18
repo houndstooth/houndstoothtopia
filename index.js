66a782f01c69603e43bb0335488beda86b19206d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-18 15:44:06 -0800

    use musical patterns version of houndstoothtopia theme

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import 'babel-polyfill'
 import populateWorld from './src/world/populateWorld'
-import startSong from './src/song/startSong'
 import attachKeyboard from './src/navigation/keyboard'
 import webVr from './src/webVr'
+import {enableImmersiveAudio, restart, setupPerformer, togglePaused} from '@musical-patterns/performer'
+import houndstoothtopiaTheme from '@musical-patterns/pattern-houndstoothtopia-theme'
 
 webVr.requestAnimationFrame()
 attachKeyboard()
 populateWorld()
 
-// contemplated adding a constant for this approx. amount that geometry takes to load,
-// but decided instead to wait for the story coming soon where the song will come out of speakers
-// which this path will just add as it needs i think
-window.setTimeout(startSong, 1000)
+setupPerformer({
+    onUpdate: () => {
+    },
+})
+enableImmersiveAudio({vrb: webVr})
+restart(houndstoothtopiaTheme)
+togglePaused()
