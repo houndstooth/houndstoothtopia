a2e1a852eada1d6008d7905702a4bc9207647455
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-30 07:59:38 -0800

    use improved interface for performer

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ import 'babel-polyfill'
 import populateWorld from './src/world/populateWorld'
 import attachKeyboard from './src/navigation/keyboard'
 import webVr from './src/webVr'
-import {enableImmersiveAudio, restart, setupPerformer, togglePaused} from '@musical-patterns/performer'
+import {setupPerformer} from '@musical-patterns/performer'
 import houndstoothtopiaTheme from '@musical-patterns/pattern-houndstoothtopia-theme'
 
 webVr.requestAnimationFrame()
 attachKeyboard()
 populateWorld()
 
-setupPerformer({
-    onUpdate: () => {
-    },
-})
-enableImmersiveAudio({vrb: webVr})
-restart(houndstoothtopiaTheme)
-togglePaused()
+setupPerformer({autoStart: {threadSpecs: houndstoothtopiaTheme, vrb: webVr}})
