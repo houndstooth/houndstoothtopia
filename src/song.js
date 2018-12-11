707342eb90aab694be41041543a254c9da537f4b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-10 21:02:22 -0800

    use peformer's ability to set home position rather than bring in the compiler just for that purpose

--- src/song.js
+++ src/song.js
@@ -1,17 +1,19 @@
-import {houndstoothtopiaThemePattern} from '@musical-patterns/pattern-houndstoothtopia-theme'
+import houndstoothtopiaTheme from '@musical-patterns/pattern-houndstoothtopia-theme'
 import webVr from './webVr'
-import {compilePattern} from '@musical-patterns/compiler'
 import {setupPerformer} from '@musical-patterns/performer'
 
 const playSong = async () => {
-    const {spec, material} = houndstoothtopiaThemePattern
-    spec.patternPositionOffset = [
-        webVr.player.position.x,
-        webVr.player.position.y,
-        webVr.player.position.z,
-    ]
-    const houndstoothtopiaTheme = await compilePattern({spec, material})
-    await setupPerformer({autoStart: {threadSpecs: houndstoothtopiaTheme, vrb: webVr}})
+    await setupPerformer({
+        autoStart: {
+            homePosition: [
+                webVr.player.position.x,
+                webVr.player.position.y,
+                webVr.player.position.z,
+            ],
+            threadSpecs: houndstoothtopiaTheme,
+            vrb: webVr
+        }
+    })
 }
 
 export {
